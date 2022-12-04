138b165b7816163f806fdb15e4f73ebf97dd45e8
Nailuj <nailuj29gaming@gmail.com>
2022-12-04 00:38:50 -0500

    Make Day 1 shorter

--- src/day1.py
+++ src/day1.py
@@ -1,5 +1,6 @@
 import utils
-def day1p1(data):
+
+def process_data(data):
     lines = data.split('\n')
     i = 0
     elves = [0]
@@ -9,23 +10,18 @@ def day1p1(data):
             elves.append(0)
         else:
             elves[i] += int(line)
+    return elves
+
+def day1p1(data):
+    elves = process_data(data)
             
     return max(elves)
 
 def day1p2(data):
-    lines = data.split('\n')
-    i = 0
-    elves = [0]
-    for line in lines:
-        if not line:
-            i += 1
-            elves.append(0)
-        else:
-            elves[i] += int(line)
+    elves = process_data(data)
             
     elves.sort(reverse=True)
     return sum(elves[0:3])
-
 
 assert day1p1(utils.read(1, True)) == 24000
 assert day1p2(utils.read(1, True)) == 45000
